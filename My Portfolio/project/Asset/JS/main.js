16943cfd47db0256254d4a502b27db7927c1bff1
Sindhu <sindhumca3@gmail.com>
2024-07-29 11:09:11 +0530

    Portfilo

---
+++ My Portfolio/project/Asset/JS/main.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".text", {
+        strings: ["Seeking a Full Stack Web Development Job"],
+        typeSpeed: 50,
+        backSpeed: 100,
+        backDelay: 5000,
+        loop: true
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".text1", {
+        strings: ["Experience And Course Details"],
+        typeSpeed: 80,
+        backSpeed: 80,
+        backDelay: 3000,
+        loop: true
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".text2", {
+        strings: ["Skill Details"],
+        typeSpeed: 80,
+        backSpeed: 80,
+        backDelay: 3000,
+        loop: true
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".text3", {
+        strings: ["Contact Me"],
+        typeSpeed: 80,
+        backSpeed: 80,
+        backDelay: 3000,
+        loop: true
+    });
+});
+
+
+
+
